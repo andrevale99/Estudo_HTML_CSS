9f8cb9e32c0fdf2f1a6f6aa503602eea5fd5f978
andrevale99 <andrefvale@gmail.com>
2023-02-08 10:43:07 -0300

    Função para calcular a média e mostrar a mensagem

--- basico/Js_com_HTML/js/funcstios.js
+++ basico/Js_com_HTML/js/funcstios.js
@@ -10,3 +10,42 @@ function reset_cnt()
     cnt = 0;
     document.getElementById("hello_p").innerHTML = "Contagem Reiniciada!";
 }
+
+//==================================================
+
+function calcular_media(p1, p2, p3, result)
+{
+    n1 = Number(document.getElementById(p1).value);
+    n2 = Number(document.getElementById(p2).value);
+    n3 = Number(document.getElementById(p3).value);
+
+    media = (n1 + n2 + n3) / 3;
+
+    if(media >= 7)
+    {
+        document.getElementById(result).classList.add("aprovado");
+
+        document.getElementById(result).classList.remove("reprovado");
+        document.getElementById(result).classList.remove("aprovado_por_nota");
+
+        document.getElementById(result).innerHTML = "Aprovado";
+    }
+    else if (media < 7 && media >= 5)
+    {
+        document.getElementById(result).classList.add("aprovado_por_nota");
+
+        document.getElementById(result).classList.remove("aprovado");
+        document.getElementById(result).classList.remove("reprovado");
+        
+        document.getElementById(result).innerHTML = "Aprovado por Nota";
+    }
+    else
+    {
+        document.getElementById(result).classList.add("reprovado");
+
+        document.getElementById(result).classList.remove("aprovado");
+        document.getElementById(result).classList.remove("aprovado_por_nota");
+
+        document.getElementById(result).innerHTML = "Reprovado";
+    }
+}
